26d267d480e35fcc917a659e8b651cdaae27f49b
PerCreate <kostya.pershin007@gmail.com>
2021-06-03 18:35:58 +0400

    feat: add few blocks and footer

--- js/page-price.js
+++ js/page-price.js
@@ -3,6 +3,16 @@ let arrowLeft = section.querySelector('.arrow-left');
 let arrowRight = section.querySelector('.arrow-right');
 
 window.addEventListener(`resize`, event => {
+    setSizeChanges();
+  }, false);
+
+const setSizeChanges = () => {
+    if(section.clientWidth > 958) {
+        let cardsContainer = section.querySelector('.cards');
+        cardsContainer.children[2].style.display = "flex";
+        cardsContainer.children[3].style.display = "flex";
+    }
+
     if(section.clientWidth <= 958 && section.clientWidth > 414) {
         let cardsContainer = section.querySelector('.cards').querySelectorAll('.price-card');
         arrowLeft.style.display = "block";
@@ -44,19 +54,42 @@ window.addEventListener(`resize`, event => {
             }
         }
     }
-    
-    if(section.clientWidth > 958) {
+    if(section.clientWidth <= 618 && section.clientWidth > 414) {
         let cardsContainer = section.querySelector('.cards');
         cardsContainer.children[2].style.display = "flex";
         cardsContainer.children[3].style.display = "flex";
+        arrowLeft.style.display = "none";
+        arrowRight.style.display = "none";
+        let currentMargin = 420;
+        let x1 = null;
+        cardsContainer.addEventListener("touchstart", (event) => {
+            x1 = event.touches[0].clientX;
+        })
+        cardsContainer.addEventListener("touchmove", (event) => {
+            let x2 = event.touches[0].clientX;
+            let xDiff = x2 - x1;
+            
+            if (currentMargin >= -420 && currentMargin <= 420) {
+                console.log(currentMargin);
+                currentMargin += xDiff / 10;
+                cardsContainer.style.transform=`translateX(${currentMargin}px)`;
+            }
+            if (currentMargin > 420) currentMargin = 420;
+            if (currentMargin < -420) currentMargin = -420;
+        })
     }
+ 
 
     if(section.clientWidth <= 414){
-        let cardsContainer = section.querySelector('.cards').querySelectorAll('.price-card');
-        cardsContainer[0].style.display = "flex";
-        cardsContainer[1].style.display = "flex";
-        cardsContainer[2].style.display = "flex";
+        let cardsContainer = section.querySelector('.cards');
+        cardsContainer.style.transform="translateX(0px)";
+        let cardsContainerArray = section.querySelector('.cards').querySelectorAll('.price-card');
+        
+        cardsContainerArray[0].style.display = "flex";
+        cardsContainerArray[1].style.display = "flex";
+        cardsContainerArray[2].style.display = "flex";
         arrowLeft.style.display = "none";
         arrowRight.style.display = "none";
     }
-  }, false);
+}
+setSizeChanges();
